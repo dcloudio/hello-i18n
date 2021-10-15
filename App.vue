66483ce70f61e5abeaa07cd8bd080f54de6b100c
handongxun <handongxun@dcloud.io>
2021-10-15 15:01:52 +0800

    init.

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+  export default {
+    onLaunch: function() {
+      console.log('App Launch')
+    },
+    onShow: function() {
+      console.log('App Show')
+    },
+    onHide: function() {
+      console.log('App Hide')
+    }
+  }
+</script>
+
+<style>
+  .container {
+    padding: 15px;
+  }
+
+  button {
+    margin-bottom: 15px;
+  }
+</style>
---
+++ README.md
@@ -0,0 +1,15 @@
+# hello-i18n
+
+# en
+
+A demo project for uni-app globalization
+
+This template include uni-framework, manifest.json, pages.json, tabbar, Page, Component, API
+
+
+# zh-hans
+
+uni-app 国际化演示
+
+包含 uni-framework、manifest.json、pages.json、tabbar、页面、组件、API
+
